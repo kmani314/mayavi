aa7b84f08bb52c6d4be6f773dcee1762a8773f44
Krishna Mani <krishnamani314@gmail.com>
2021-01-11 13:36:27 -0800

    make installable

--- mayavi/__init__.py
+++ mayavi/__init__.py
@@ -15,7 +15,6 @@ __requires__ = [
     'pygments',  # This is only needed for the Qt backend but we add it anyway.
     'traits>=6.0.0',
     'traitsui>=7.0.0',
-    'vtk'
 ]
 
 __extras_require__ = {
